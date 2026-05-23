3022f37cd660d2dfb1e68744d078a307c033dae7
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2026-05-23 11:32:49 -0500

    se agregó el módulo de HealthCheck con su controlador y pruebas unitarias

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common/exceptions/rpc-custom-exception.filter';
 
 async function bootstrap() {
   const logger = new Logger('Main-Gateway');
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET
+      }
+    ]
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,7 +24,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.PORT);
-  // console.log('Hola mundo segundo cambio');
+  console.log('health check configured');
   logger.log(`Gateway running on port ${envs.PORT}`);
 }
 bootstrap();
